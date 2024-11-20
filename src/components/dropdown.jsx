67ac4e940484988e9926ae7747b7ff92ed30033d
boramerts <mertbora723@gmail.com>
2024-11-20 23:07:29 +0300

    UI WIP

--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,73 +1,78 @@
 import React from "react";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
+import ChevronDown from "@mui/icons-material/KeyboardArrowDownOutlined";
+
 export default function Dropdown() {
   return (
     <Menu as="div" className="relative inline-block text-left">
       {/* Dropdown Button */}
       <MenuButton
-        style={{ width: "20rem", height: "40px" }} // Equivalent to w-96
-        className="inline-flex items-start justify-between px-4 py-3 bg-white text-gray-900 text-sm font-medium rounded-lg shadow-md ring-1 ring-gray-300 hover:bg-gray-100 focus:outline-none"
+        style={{ width: "10rem", height: "40px" }} // Equivalent to w-96
+        className="flex items-center justify-between px-4 py-3 bg-white text-gray-900 text-sm font-medium rounded-lg shadow-md ring-1 ring-gray-300 hover:bg-gray-100 focus:outline-none"
       >
-        <p className="text-left">Options</p>
-        <img
+        <p className="text-left flex-grow">Custom</p>
+        {/* <img
           src="src/assets/chevron.down.svg"
           alt="Dropdown Icon"
-          style={{width: "1rem", height: "1rem"}}
-          className=""
-        />
+          style={{ width: "1rem", height: "1rem" }}
+          className="ml-2"
+        /> */}
+        <ChevronDown></ChevronDown>
       </MenuButton>
 
       {/* Dropdown Menu */}
-      <MenuItems className="absolute right-0 mt-2 !w-96 bg-white shadow-lg rounded-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-        <MenuItem>
-          {({ active }) => (
-            <a
-              href="#"
-              className={`block px-4 py-2 text-sm ${
-                active ? "bg-gray-100 text-gray-900" : "text-gray-700"
-              }`}
-            >
-              Account settings
-            </a>
-          )}
-        </MenuItem>
-        <MenuItem>
-          {({ active }) => (
-            <a
-              href="#"
-              className={`block px-4 py-2 text-sm ${
-                active ? "bg-gray-100 text-gray-900" : "text-gray-700"
-              }`}
-            >
-              Support
-            </a>
-          )}
-        </MenuItem>
-        <MenuItem>
-          {({ active }) => (
-            <a
-              href="#"
-              className={`block px-4 py-2 text-sm ${
-                active ? "bg-gray-100 text-gray-900" : "text-gray-700"
-              }`}
-            >
-              License
-            </a>
-          )}
-        </MenuItem>
-        <MenuItem>
-          {({ active }) => (
-            <button
-              type="button"
-              className={`block w-full text-left px-4 py-2 text-sm ${
-                active ? "bg-gray-100 text-gray-900" : "text-gray-700"
-              }`}
-            >
-              Sign out
-            </button>
-          )}
-        </MenuItem>
+      <MenuItems className="absolute right-0 mt-2 w-96 bg-white shadow-lg rounded-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div className="flex flex-col">
+          <MenuItem>
+            {({ active }) => (
+              <a
+                href="#"
+                className={`block w-full text-left px-4 py-2 text-sm ${
+                  active ? "bg-gray-100 text-gray-900" : "text-gray-700"
+                }`}
+              >
+                Model1
+              </a>
+            )}
+          </MenuItem>
+          <MenuItem>
+            {({ active }) => (
+              <a
+                href="#"
+                className={`block w-full text-left px-4 py-2 text-sm ${
+                  active ? "bg-gray-100 text-gray-900" : "text-gray-700"
+                }`}
+              >
+                Model2
+              </a>
+            )}
+          </MenuItem>
+          <MenuItem>
+            {({ active }) => (
+              <a
+                href="#"
+                className={`block w-full text-left px-4 py-2 text-sm ${
+                  active ? "bg-gray-100 text-gray-900" : "text-gray-700"
+                }`}
+              >
+                Model3
+              </a>
+            )}
+          </MenuItem>
+          <MenuItem>
+            {({ active }) => (
+              <button
+                type="button"
+                className={`block w-full text-left px-4 py-2 text-sm ${
+                  active ? "bg-gray-100 text-gray-900" : "text-gray-700"
+                }`}
+              >
+                Model4
+              </button>
+            )}
+          </MenuItem>
+        </div>
       </MenuItems>
     </Menu>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,35 @@ import ReactDOM from "react-dom/client";
 import Dropdown from "./components/dropdown";
 import "./output.css"; // Include your Tailwind styles
 
+import FileIcon from "@mui/icons-material/NoteAddOutlined";
+import SaveIcon from "@mui/icons-material/SaveOutlined";
+import ExportIcon from "@mui/icons-material/FileDownloadOutlined";
+
 const App = () => (
   <div className="flex flex-col justify-between min-h-screen px-20 py-10">
-    <div className="w-full h-20 bg-gray-300 rounded-3xl border-2 border-gray-400 flex flex-row items-center justify-start">
-      <p className="text-black p-5 text-center text-2xl font-bold">Model Builder</p>
-      <p className="text-black p-5 text-center font-normal">Preset</p>
+    <div className="w-full h-20 bg-gray-300 rounded-3xl border-2 border-gray-400 flex flex-row items-center justify-between">
+      <div className="w-1/2 h-20 flex flex-row items-center justify-start p-5">
+        <p className="text-black text-center text-2xl font-bold">
+          Model Builder
+        </p>
+        <p className="text-black text-center font-normal p-5">Preset:</p>
+        <Dropdown></Dropdown>
+      </div>
 
-      <Dropdown></Dropdown>
-
-      <p className="text-black p-5 text-center font-bold">Top Rectangle</p>
+      <div className="w-1/2 h-20 flex flex-row items-center justify-end p-5 space-x-3">
+        <button className="px-4 py-3 bg-gray-200 rounded-xl hover:bg-gray-500 space-x-2 flex flex-row">
+          <FileIcon></FileIcon>
+          <p>New</p>
+        </button>
+        <button className="px-4 py-3 bg-gray-200 rounded-xl hover:bg-gray-500 space-x-2 flex flex-row">
+          <SaveIcon></SaveIcon>
+          <p>Save</p>
+        </button>
+        <button className="px-4 py-3 bg-gray-200 rounded-xl hover:bg-gray-500 space-x-2 flex flex-row">
+          <ExportIcon></ExportIcon>
+          <p>Export</p>
+        </button>
+      </div>
     </div>
 
     <div className="flex-1"></div>
